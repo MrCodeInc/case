0965fb6c94ff8778bb410f02a445ec38cf092c8d
MrCode <MrCodeInc@users.noreply.github.com>
2016-12-01 17:40:07 -0500

    Update case.php

--- case.php
+++ case.php
@@ -9,9 +9,10 @@ $messages = array(
     'You got a php code!',
     'You got two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda!',
     'You got a MrCodeInc!',
-    'You got a meme!'
+    'You got a meme!',
+    'You got nothing.'
 );
-echo $messages[rand(0, count($messages) - 10)];
+echo $messages[rand(0, count($messages) - 1)];
 ?>
 
 <form action="index.php" method="get">
